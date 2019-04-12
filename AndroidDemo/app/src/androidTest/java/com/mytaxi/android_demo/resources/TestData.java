13ee63142518124ff369ae9d7f97385196127850
rranchana <48172571+rranchana@users.noreply.github.com>
2019-04-12 12:32:43 +0530

    Update TestData.java

--- AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/resources/TestData.java
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/resources/TestData.java
@@ -3,7 +3,7 @@ package com.mytaxi.android_demo.resources;
 public class TestData {
 
     public static final String userName = "crazydog335";
-    public static final String password = "venture";
+    public static final String password = "vEnture";
     public static final String searchText = "sa";
     public static final String driverName = "Sarah Scott";
 
